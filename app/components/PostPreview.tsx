59848e2e7f36a0ea988be894fdf4505cac6e135b
adehenry <adefemiadelaja@gmail.com>
2023-03-02 22:02:10 +0100

    added title border

--- app/components/PostPreview.tsx
+++ app/components/PostPreview.tsx
@@ -7,7 +7,9 @@ export default function PostPreview(props: PostMetadata) {
     <div key={props.slug} className='border border-slate-300 p-4 rounded-md shadow-md bg-white'>
     <p className='text-sm text-slate-400'>{props.date}</p>
     <Link href={`/posts/${props.slug}`}>
-      <h2 className='font-bold text-violet-400 mb-4 hover:underline hover:text-violet-500'>{props.title}</h2>
+      <h2 className='bg-slate-800 font-bold border text-white mb-4 py-1 px-2 rounded-md w-max hover:bg-transparent hover:text-slate-800 hover:border-slate-800'>
+        {props.title}
+      </h2>
     </Link>
     <p className='text-slate-700'>{props.subtitle}</p>
   </div>
